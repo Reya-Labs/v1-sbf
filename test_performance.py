3318d8d81c1c041a9e3e6ffa1eb7a934c29130dd
Artur Begyan <arturbegyan98@gmail.com>
2022-06-09 01:41:46 +0100

    test performance metrics calculator unit test setup

---
+++ test_performance.py
@@ -0,0 +1,14 @@
+import unittest
+from portfolio import NaivePortfolio
+from data import HistoricCSVDataHandler
+from event import SignalEvent, FillEvent
+import queue
+from datetime import datetime
+
+class TestPerformanceMetricsCalculator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
