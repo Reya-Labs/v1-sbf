64b270c0ea9c3c281a6e718d0ccc8e47f81150b5
Mike Nelson <mikenelson@Mikes-MacBook-Pro-3.local>
2022-06-14 15:06:05 +0100

    Adding momentum backtest

--- backtest.py
+++ backtest.py
@@ -2,7 +2,7 @@ from event_loop import EventLoop
 import queue
 from abc import ABCMeta, abstractmethod
 from data import HistoricCSVDataHandler
-from strategy import LongRateStrategy
+from strategy import LongRateStrategy, LongShortMomentumStrategy
 from execution import SimulatedExecutionHandler
 from portfolio import NaivePortfolio
 
@@ -16,6 +16,59 @@ class Backtest(object):
     def run_backtest(self):
 
         raise NotImplementedError("Should implement run_backtest()")
+
+class LongShortMomentumStrategyBacktest(Backtest):
+
+    def __init__(self, start_date_time='2022-04-01 00:00:00',
+                 end_date_time='2022-06-01 00:00:00',
+                 leverage=1.0, initial_capital=1.0,
+                 lookback=30, buffer=1):
+
+        self.events_queue = queue.Queue()
+
+        self.dataHandler = HistoricCSVDataHandler(
+            events=self.events_queue,
+            csv_dir="datasets",
+            token_list=["aave_usdc"],
+            start_date_time=start_date_time,
+            end_date_time=end_date_time
+        )
+
+        self.strategy = LongShortMomentumStrategy(
+            rates=self.dataHandler,
+            events=self.events_queue,
+            lookback=lookback,
+            buffer=buffer
+        )
+
+        self.portfolio = NaivePortfolio(
+            rates=self.dataHandler,
+            events=self.events_queue,
+            start_date_time=start_date_time,
+            leverage=leverage,
+            initial_capital=initial_capital
+        )
+
+        self.executionHandler = SimulatedExecutionHandler(
+            events=self.events_queue
+        )
+
+        # one day needs to pass to enable fixed rate calculations which needs at least two historical rate values
+        self.dataHandler.update_rates()
+
+        self.eventLoop = EventLoop(
+            events=self.events_queue,
+            rates=self.dataHandler,
+            strategy=self.strategy,
+            portfolio=self.portfolio,
+            executionHandler=self.executionHandler
+        )
+
+    def run_backtest(self):
+
+        self.eventLoop.run_outer_loop()
+
+        return self.eventLoop.portfolio
 
 
 class LongRateStrategyBacktest(Backtest):
