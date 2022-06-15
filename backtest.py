d33063d62a8e4336e1d5d895a29ecc54b76d284c
Mike Nelson <mikenelson@Mikes-MacBook-Pro-3.local>
2022-06-15 18:28:12 +0100

    Adding stat arb backtester class

--- backtest.py
+++ backtest.py
@@ -2,7 +2,7 @@ from event_loop import EventLoop
 import queue
 from abc import ABCMeta, abstractmethod
 from data import HistoricCSVDataHandler
-from strategy import LongRateStrategy, LongShortMomentumStrategy
+from strategy import LongRateStrategy, LongShortMomentumStrategy, StatisticalArbitragePairs
 from execution import SimulatedExecutionHandler
 from portfolio import NaivePortfolio
 
@@ -16,6 +16,56 @@ class Backtest(object):
     def run_backtest(self):
 
         raise NotImplementedError("Should implement run_backtest()")
+
+class LongRateStrategyBacktest(Backtest):
+
+    def __init__(self, start_date_time='2022-04-01 00:00:00',
+                 end_date_time='2022-06-01 00:00:00',
+                 leverage=1.0, initial_capital=1.0):
+
+        self.events_queue = queue.Queue()
+
+        self.dataHandler = HistoricCSVDataHandler(
+            events=self.events_queue,
+            csv_dir="datasets",
+            token_list=["aave_usdc"],
+            start_date_time=start_date_time,
+            end_date_time=end_date_time
+        )
+
+        self.strategy = LongRateStrategy(
+            rates=self.dataHandler,
+            events=self.events_queue
+        )
+
+        self.portfolio = NaivePortfolio(
+            rates=self.dataHandler,
+            events=self.events_queue,
+            start_date_time=start_date_time,
+            leverage=leverage,
+            initial_capital=initial_capital
+        )
+
+        self.executionHandler = SimulatedExecutionHandler(
+            events=self.events_queue
+        )
+
+        # one day needs to pass to enable fixed rate calculations which needs at least two historical rate values
+        self.dataHandler.update_rates()
+
+        self.eventLoop = EventLoop(
+            events=self.events_queue,
+            rates=self.dataHandler,
+            strategy=self.strategy,
+            portfolio=self.portfolio,
+            executionHandler=self.executionHandler
+        )
+
+    def run_backtest(self):
+
+        self.eventLoop.run_outer_loop()
+
+        return self.eventLoop.portfolio
 
 class LongShortMomentumStrategyBacktest(Backtest):
 
@@ -71,26 +121,31 @@ class LongShortMomentumStrategyBacktest(Backtest):
 
         return self.eventLoop.portfolio
 
+class StatisticalArbitragePairsBacktest(Backtest):
 
-class LongRateStrategyBacktest(Backtest):
-
-    def __init__(self, start_date_time='2022-04-01 00:00:00',
-                 end_date_time='2022-06-01 00:00:00',
-                 leverage=1.0, initial_capital=1.0):
+    def __init__(self, start_date_time="2022-04-01 00:00:00",
+                 end_date_time="2022-06-01 00:00:00",
+                 leverage=1.0, initial_capital=1.0,
+                 lookback_window=30, apy_lookback=1, deviations=1,
+                 pairs = [("aave_usdc", "aave_dai")]):
 
         self.events_queue = queue.Queue()
 
         self.dataHandler = HistoricCSVDataHandler(
             events=self.events_queue,
             csv_dir="datasets",
-            token_list=["aave_usdc"],
+            token_list=["aave_usdc", "aave_dai"], # aUSDC - aDAI arbitrage 
             start_date_time=start_date_time,
             end_date_time=end_date_time
         )
 
-        self.strategy = LongRateStrategy(
+        self.strategy = StatisticalArbitragePairs(
             rates=self.dataHandler,
-            events=self.events_queue
+            events=self.events_queue,
+            lookback_window=lookback_window,
+            apy_lookback=apy_lookback,
+            deviations=deviations,
+            pairs=pairs
         )
 
         self.portfolio = NaivePortfolio(
