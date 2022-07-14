e30321603ab22cb2b9f7b7dc02a8467e8364d4f8
Mike Nelson <mikenelson@Mikes-MacBook-Pro-3.local>
2022-07-14 16:31:50 +0100

    Reverting to an alpha version of the sbf

--- backtest.py
+++ backtest.py
@@ -3,8 +3,6 @@ import queue
 from abc import ABCMeta, abstractmethod
 from data import HistoricCSVDataHandler
 from strategy import LongRateStrategy
-from LongShortMomentum import LongShortMomentumStrategy
-from StatisticalArbitragePairs import StatisticalArbitragePairs
 from execution import SimulatedExecutionHandler
 from portfolio import NaivePortfolio
 import datetime
@@ -65,128 +63,6 @@ class LongRateStrategyBacktest(Backtest):
         )
 
     def run_backtest(self):
-        self.eventLoop.run_outer_loop()
-
-        return self.eventLoop.portfolio
-
-class LongShortMomentumStrategyBacktest(Backtest):
-
-    def __init__(self, start_date_time="2022-04-01 00:00:00",
-                 end_date_time="2022-06-01 00:00:00",
-                 leverage=1.0, initial_capital=1.0,
-                 trend_lookback=15, apy_lookback=5, buffer=1,
-                 trade_trend=False):
-
-        self.events_queue = queue.Queue()
-
-        self.dataHandler = HistoricCSVDataHandler(
-            events=self.events_queue,
-            csv_dir="datasets",
-            token_list=["rocket_rETH"], 
-            start_date_time=start_date_time,
-            end_date_time=end_date_time,
-            is_liquid_staking=False
-        )
-
-        self.strategy = LongShortMomentumStrategy(
-            rates=self.dataHandler,
-            events=self.events_queue,
-            trend_lookback=trend_lookback,
-            apy_lookback=apy_lookback,
-            buffer=buffer,
-            trade_trend=trade_trend
-        )
-
-        self.portfolio = NaivePortfolio(
-            rates=self.dataHandler,
-            events=self.events_queue,
-            start_date_time=start_date_time,
-            leverage=leverage,
-            initial_capital=initial_capital
-        )
-
-        self.executionHandler = SimulatedExecutionHandler(
-            events=self.events_queue
-        )
-
-        # one day needs to pass to enable fixed rate calculations which needs at least two historical rate values
-        self.dataHandler.update_rates()
-
-        self.eventLoop = EventLoop(
-            events=self.events_queue,
-            rates=self.dataHandler,
-            strategy=self.strategy,
-            portfolio=self.portfolio,
-            executionHandler=self.executionHandler
-        )
-
-    def run_backtest(self):
-
-        self.eventLoop.run_outer_loop()
-
-        return self.eventLoop.portfolio
-
-class StatisticalArbitragePairsBacktest(Backtest):
-
-    def __init__(self, start_date_time="2022-04-01 00:00:00",
-                 end_date_time="2022-06-01 00:00:00",
-                 leverage=1.0, initial_capital=1.0,
-                 lookback_window=30, apy_lookback=5, deviations=1,
-                 pairs = [("eth_ceth", "rocket_rETH")],
-                 monthly=False):
-
-        self.events_queue = queue.Queue()
-
-        # We need to play around with datetime, and make sure that the stat arb lookback
-        # window is still being picked up 
-        read_date = datetime.datetime.strptime(start_date_time, "%Y-%m-%d 00:00:00")
-        get_lookback = read_date - datetime.timedelta(days=lookback_window)
-        formatted_start_date = get_lookback.strftime("%Y-%m-%d 00:00:00")
-
-        self.dataHandler = HistoricCSVDataHandler(
-            events=self.events_queue,
-            csv_dir="datasets",
-            token_list=list(pairs[0]),  
-            start_date_time=formatted_start_date,
-            end_date_time=end_date_time
-        )
-        
-        self.strategy = StatisticalArbitragePairs(
-            rates=self.dataHandler,
-            events=self.events_queue,
-            lookback_window=lookback_window,
-            apy_lookback=apy_lookback,
-            deviations=deviations,
-            pairs=pairs,
-            strategy_start=start_date_time, # True start of the analysis, so we can collect lookback window data first
-            monthly=monthly
-        )
-
-        self.portfolio = NaivePortfolio(
-            rates=self.dataHandler,
-            events=self.events_queue,
-            start_date_time=start_date_time,
-            leverage=leverage,
-            initial_capital=initial_capital
-        )
-
-        self.executionHandler = SimulatedExecutionHandler(
-            events=self.events_queue
-        )
-
-        # one day needs to pass to enable fixed rate calculations which needs at least two historical rate values
-        self.dataHandler.update_rates()
-
-        self.eventLoop = EventLoop(
-            events=self.events_queue,
-            rates=self.dataHandler,
-            strategy=self.strategy,
-            portfolio=self.portfolio,
-            executionHandler=self.executionHandler
-        )
-
-    def run_backtest(self):
-
         self.eventLoop.run_outer_loop()
 
         return self.eventLoop.portfolio
--- run_strategy_backtester.py
+++ run_strategy_backtester.py
@@ -1,32 +1,10 @@
-from calendar import month
-from backtest import LongShortMomentumStrategyBacktest as LSM
-from backtest import StatisticalArbitragePairsBacktest as SAP
 from backtest import LongRateStrategyBacktest as LR
-from data import DataHandler
-from reporter import SimpleBacktestReporter
 import os
-import optuna
-import numpy as np
-import json
 
-RUN_OPTUNA = False
 def main(start_date_time="2021-04-01 00:00:00", end_date_time="2022-06-01 00:00:00", leverage=1.0, \
-            initial_capital=1.0, trend_lookback=15, apy_lookback=5, buffer=1.0, trade_trend=False,
-            trade_stat_arb=False, deviations=1.0, lookback_window=30, monthly=False, long_rate=False):
-
-    backtest = LSM(start_date_time=start_date_time,
-                 end_date_time=end_date_time,
-                 leverage=leverage, initial_capital=initial_capital,
-                 trend_lookback=trend_lookback, apy_lookback=apy_lookback, buffer=buffer,
-                 trade_trend=trade_trend)
-    if trade_stat_arb:
-        backtest = SAP(start_date_time=start_date_time, end_date_time=end_date_time, leverage=leverage,\
-                       initial_capital=initial_capital, lookback_window=lookback_window, \
-                       apy_lookback=apy_lookback, deviations=deviations, monthly=monthly)
-
-    if long_rate:
-        backtest = LR(start_date_time=start_date_time,end_date_time=end_date_time,\
-            leverage=leverage, initial_capital=initial_capital)
+            initial_capital=1.0):
+    
+    backtest = LR(start_date_time=start_date_time,end_date_time=end_date_time, leverage=leverage, initial_capital=initial_capital)
 
     # Run the backtest and get the output portfolio object
     output_portfolio = backtest.run_backtest()
@@ -41,98 +19,25 @@ def main(start_date_time="2021-04-01 00:00:00", end_date_time="2022-06-01 00:00:
     end = end_date_time.split(" ")[0]
     if not os.path.exists(f"./reports/{end}"):
         os.makedirs(f"./reports/{end}")
-    name = output_portfolio.equity_curve.columns[3]
-    if trade_trend:
-        output_portfolio.equity_curve.to_csv(f"./reports/{end}/df_LongShortMomentum_TREND_{name}_lookback_{trend_lookback}days_apy_lookback_{apy_lookback}_buffer_{buffer}_leverage_{leverage}.csv")
-    elif trade_stat_arb:
-        if monthly:
-            output_portfolio.equity_curve.to_csv(f"./reports/{end}/df_StatArb_Monthly_{name}_lookback_{lookback_window}days_apy_lookback_{apy_lookback}_deviations_{deviations}_leverage_{leverage}.csv")
-        else:
-            output_portfolio.equity_curve.to_csv(f"./reports/{end}/df_StatArb_Rolling_{name}_lookback_{lookback_window}days_apy_lookback_{apy_lookback}_deviations_{deviations}_leverage_{leverage}.csv")
-    elif long_rate:
-        output_portfolio.equity_curve.to_csv(f"./reports/{end}/df_LongOnly_{name}_lookback_{trend_lookback}days_apy_lookback_{apy_lookback}_buffer_{buffer}_leverage_{leverage}.csv")
-    else:
-        output_portfolio.equity_curve.to_csv(f"./reports/{end}/df_LongShortMomentum_RATE_{name}_lookback_{trend_lookback}days_apy_lookback_{apy_lookback}_buffer_{buffer}_leverage_{leverage}.csv")
+    output_portfolio.equity_curve.to_csv(f"./reports/{end}/df_LongOnly_leverage_{leverage}.csv")
 
     print("Backtest summary:")
     print(stats)
     for s in stats:
         print(s[0]+ ": "+s[1])
 
-    if RUN_OPTUNA:
-        obj = float(stats[1][1]) - float(stats[2][1][:-1])
-        return obj 
-
-def objective(trial):
-    
-    trend_lookback = trial.suggest_categorical("trend_lookback", np.arange(3, 50, 1).tolist())
-    apy_lookback = trial.suggest_categorical("apy_lookback", np.arange(3, 50, 1).tolist())
-    buffer = trial.suggest_categorical("buffer", np.linspace(0.0, 2, 2000).tolist())
-
-    obj = main(start_date_time="2021-04-01 00:00:00", end_date_time="2022-02-01 00:00:00", leverage=1.0, \
-            initial_capital=1.0, trend_lookback=trend_lookback, apy_lookback=apy_lookback, buffer=buffer)
-
-    return obj
-
-def run_single(parser):
+if __name__=="__main__":
+    # Adding an argument parser
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
     
     parser.add_argument("-sd", "--start_date_time", type=str, help="Starting date for backtest", default="2021-04-01 00:00:00")
     parser.add_argument("-ed", "--end_date_time", type=str, help="Ending date for backtest", default="2022-06-01 00:00:00")
     parser.add_argument("-l", "--leverage", type=float, help="Leverage (notional / margin)", default=1.0)
     parser.add_argument("-i", "--initial_capital", type=float, help="Initial capital (notional before leverage)", default=1.0)
-    parser.add_argument("-tl", "--trend_lookback", type=int, help="Lookback window for momentum following (e.g. days)", default=15)
-    parser.add_argument("-lw", "--lookback_window", type=int, help="Lookback window for statistical arbitrage", default=30)
-    parser.add_argument("-al", "--apy_lookback", type=int, help="Lookback window for converting liquidity index to APY", default=5)
-    parser.add_argument("-b", "--buffer", type=float, help="Buffer to apply to momentum spread in lookback window", default=1.0)
-    parser.add_argument("-d", "--deviations", type=float, help="Buffer to apply to stat arb signal", default=1.0)
-    parser.add_argument("-t", "--trade_trend", action="store_true", help="Run simple trend-following strategy", default=False)
-    parser.add_argument("-sa", "--trade_stat_arb", action="store_true", help="Run the statistical arbitrage pairs trade", default=False)
-    parser.add_argument("-m", "--monthly", action="store_true", help="Run the statistical arbitrage pairs trade under a monthly strategy", default=False)
-    parser.add_argument("-lr", "--long_rate", action="store_true", help="VT hold i.e. long rate strategy only", default=False)
 
     params = parser.parse_args()
     param_dict = dict((k, v) for k, v in vars(params).items() if v is not None)
     print(param_dict)
     
     main(**param_dict)
-
-def run_optimisation(parser):
-
-    parser.add_argument("-n_trials", "--n_trials", type=float, help="Number of optimization trials", default=2)
-    n_trials = parser.parse_args().n_trials
-
-    study = optuna.create_study(direction="maximize", sampler=optuna.samplers.TPESampler(), pruner=optuna.pruners.SuccessiveHalvingPruner())
-    study.optimize(objective, n_trials=n_trials)
-    
-    # Relevant output plots
-    out_dir = "./Momentum_optimisation/"
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    # Output optimised results
-    trial = study.best_trial
-    print(f"Best optimised value: {trial.value}")
-
-    print("Optimised parameters: ")
-    for key, value in trial.params.items():
-        print(f"{key}: {value}")
-    
-    fig = optuna.visualization.plot_optimization_history(study)
-    fig.write_image(out_dir+f"optuna_history.png")
-
-    fig = optuna.visualization.plot_param_importances(study)
-    fig.write_image(out_dir+f"optuna_importances.png")
-    
-    with open(out_dir+f"optimised_parameters.json", "w") as fp:
-        json.dump(trial.params, fp, indent=4)
-
-if __name__=="__main__":
-    # Adding an argument parser
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-
-    if RUN_OPTUNA:
-        run_optimisation(parser=parser)
-    else:
-        run_single(parser=parser)
-    
